0e3f4af7a88e1662980168d275424439ed64a4d2
k0nAtbl4 <gsbeshsvzb620@gmail.com>
2022-08-24 23:19:10 +0300

    init

---
+++ scheme.ts
@@ -0,0 +1,36 @@
+type Image = string
+type Role = 'Admin' | 'Moderator' | 'User' 
+interface User {
+    name: string
+    surname: string
+    age: number
+    link?: string
+    password: string
+    photo?: Image
+    role: Role[]
+    points: number
+    rank: number
+    submitTasks: number
+    submission: TaskSubmission[]
+}
+type Code = {
+    text: string
+    language: 'js' | 'ts'
+}
+type Opinion = 'like' | 'dislike'
+type TaskSubmission = {
+    user: User
+    task: Task
+    status: TaskSubmissionStatus
+    code: Code
+    opinion?: Opinion
+}
+type TaskSubmissionStatus = 'success' | 'fail'
+interface Task{
+    users: TaskSubmission[]
+    tests: Code
+    description: string
+    level: number
+    startCode: Code[]
+    
+}
