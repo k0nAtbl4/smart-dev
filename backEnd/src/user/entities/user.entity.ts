b4c2be04ccfbe9ed9f54977d3ffcb48e6e7a71f9
k0nAtbl4 <gsbeshsvzb620@gmail.com>
2022-08-27 03:53:12 +0300

    user :(

---
+++ backEnd/src/user/entities/user.entity.ts
@@ -0,0 +1,55 @@
+// interface User {
+//     name: string
+//     surname: string
+//     age: number
+//     link?: string
+//     password: string
+//     photo?: Image
+//     role: Role[]
+//     points: number
+//     rank: number
+//     submitTasks: number
+//     submission: TaskSubmission[]
+//     }
+import { TaskSubmission } from "src/taskSubmision/entities/taskSubmission.entity";
+import { Column, Entity, ManyToMany, ManyToOne, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+
+export class User {
+    @Column()
+    name: string;
+
+    @Column()
+    surname: string;
+
+    @Column()
+    age: number;
+
+    @Column()
+    link?: string
+    
+    @Column()
+    password: string;
+
+    @Column()
+    photo?: string; // image
+
+    @Column()
+    role: string; // Role[]
+
+    @Column()
+    points: number;
+
+    @Column()
+    rnak: number;
+
+    @Column()
+    submitTasks: number
+
+    @OneToMany(type => User, (user) => user.submission, {eager: true})
+    submission: TaskSubmission[]
+
+    @OneToMany(type => Task, (task) => task.users , {eager: true})
+    users: TaskSubmission[]
+}
